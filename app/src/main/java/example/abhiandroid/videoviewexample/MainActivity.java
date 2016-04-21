334990783ca5870b03babef4b7cff0c04a62879e
Abhishek Saini <saini.abhishek.awake@gmail.com>
2016-04-21 11:49:48 +0530

    Initial commit

---
+++ app/src/main/java/example/abhiandroid/videoviewexample/MainActivity.java
@@ -0,0 +1,51 @@
+package example.abhiandroid.videoviewexample;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+    VideoView simpleVideoView;
+    MediaController mediaControls;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Find your VideoView in your video_main.xml layout
+        simpleVideoView = (VideoView) findViewById(R.id.simpleVideoView);
+
+        if (mediaControls == null) {
+            // create an object of media controller class
+            mediaControls = new MediaController(MainActivity.this);
+            mediaControls.setAnchorView(simpleVideoView);
+        }
+        // set the media controller for video view
+        simpleVideoView.setMediaController(mediaControls);
+        // set the uri for the video view
+        simpleVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.fishvideo));
+        // start a video
+        simpleVideoView.start();
+
+        // implement on completion listener on video view
+        simpleVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(getApplicationContext(), "Thank You...!!!", Toast.LENGTH_LONG).show(); // display a toast when an video is completed
+            }
+        });
+        simpleVideoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Toast.makeText(getApplicationContext(), "Oops An Error Occur While Playing Video...!!!", Toast.LENGTH_LONG).show(); // display a toast when an error is occured while playing an video
+                return false;
+            }
+        });
+    }
+
+}
